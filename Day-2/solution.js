518b2303115117a75be5fea6ec54e713b33c3feb
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-23 23:00:37 +0530

    Day 2 question 3

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -1,5 +1,6 @@
 // write all your solution here. 
 
+// solution 1
 function question1(input){
    if(Array.isArray(input)){
        return true;
@@ -7,6 +8,7 @@ function question1(input){
    return false;
 }
 
+// solution 2
 function question2(arr1 , arr2){
     const arr3 = []
     arrayCount = 0
@@ -36,11 +38,23 @@ function question2(arr1 , arr2){
     return (arr3)
 }
 
+// solution 3
+function question3(arr){
+    const alteredArr = []
+    arr.map(str => {
+        str = str.replace(/-/g , '/')
+        alteredArr.push(str)
+    });
+    return alteredArr;
+}
+
+// solution 4
 function question4(arr){
     arr2 = arr.map(num => Math.ceil(num))
     return arr2
 }
 
+// solution 5
 // using filter
 function question5(arr){
     filteredArray = arr.filter(num => num > 0);
@@ -62,6 +76,7 @@ function filter(arr){
 const answer1 = question1([1, 2, 4, 0]);
 const answer1_2 = question1('w3resource');
  const answer2 = question2([1,0,2,3,4,] , [3,5,6,7,8,13]);
+ const answer3 = question3(['08-14', '10-04', '04-21']);
  const answer4 = question4([1.5, 2.56, 5.1, 12.33]);
  const answer5 = question5([-4, 3, 2, -21, 1]);
  const answer5_2 = filter([-4, 3, 2, -21, 1]);
@@ -70,6 +85,7 @@ const answer1_2 = question1('w3resource');
 console.log(answer1);
 console.log(answer1_2);
  console.log(answer2);
+ console.log(answer3)
 console.log(answer4);
 console.log(answer5);
 console.log(answer5_2);
