beaba3909ba9d40806c071698e0aa7ec25edffab
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-23 22:49:35 +0530

    Day 2 question 5

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -41,8 +41,20 @@ function question4(arr){
     return arr2
 }
 
-function question5(){
-    
+// using filter
+function question5(arr){
+    filteredArray = arr.filter(num => num > 0);
+    return filteredArray;
+}
+// using map
+function filter(arr){
+    const filarr = []
+     arr.map(num => {
+        if (num > 0){
+            filarr.push(num)
+        }
+    });
+    return filarr;
 }
 
 
@@ -51,9 +63,13 @@ const answer1 = question1([1, 2, 4, 0]);
 const answer1_2 = question1('w3resource');
  const answer2 = question2([1,0,2,3,4,] , [3,5,6,7,8,13]);
  const answer4 = question4([1.5, 2.56, 5.1, 12.33]);
+ const answer5 = question5([-4, 3, 2, -21, 1]);
+ const answer5_2 = filter([-4, 3, 2, -21, 1]);
 
 
 console.log(answer1);
 console.log(answer1_2);
  console.log(answer2);
 console.log(answer4);
+console.log(answer5);
+console.log(answer5_2);
