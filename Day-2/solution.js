ecbfac530ca15ac8e098aec89a07f1dd25ac3967
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-23 21:08:47 +0530

    Day 2 question 1

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -1,7 +1,10 @@
 // write all your solution here. 
 
-function question1(){
-
+function question1(input){
+   if(Array.isArray(input)){
+       return true;
+   }
+   return false;
 }
 
 function question2(){
@@ -14,11 +17,13 @@ function question3(){
 
 
 
-const answer1 = question1('pass sample input');
-const answer2 = question2('pass sample input');
-const answer3 = question3('pass sample input');
+const answer1 = question1([1, 2, 4, 0]);
+const answer1_2 = question1('w3resource');
+// const answer2 = question2('pass sample input');
+// const answer3 = question3('pass sample input');
 
 
 console.log(answer1);
-console.log(answer2);
-console.log(answer3);
+console.log(answer1_2);
+// console.log(answer2);
+// console.log(answer3);
