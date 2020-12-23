512e33d9aa1222816fb8cbc3f0a9d2d901d2f70a
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-23 20:37:55 +0530

    Day 1 completed

--- Day-1/solution.js
+++ Day-1/solution.js
@@ -1,23 +1,60 @@
 // write all your solution here. 
 
-function question1(){
+function question1(val){
+    if(val !== false || val!== null || val!==0 || val!=='')
+    return val;
+}
+const valArray = [58, '', 'abcd', true, null, false, 0]
 
+// Solution 2
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+    ];
+
+function question2(a , b){
+    let ftitle = a.title.toLowerCase();
+    let stitle = b.title.toLowerCase();
+    if(ftitle < stitle){
+        return -1
+    }
+    if(ftitle > stitle){
+        return 1
+    }
+
+    return 0
 }
 
-function question2(){
+// Solution 3
+function question3(number){
+   const str = number.toString();  
+   let numArray = [];
+    for(let i=0; i<str.length; i++)
+  {
+    if((str[i+1]%2 === 0 && str[i]%2 === 0))
+     {
+        numArray.push(str[i] , '-');
+     }
+    else
+     {
+        numArray.push(str[i]);
+     }
+  }
+    return (numArray.join(''))
+    }
 
-}
-
-function question3(){
-
-}
+    
+ 
 
 
 
-const answer1 = question1('pass sample input');
-const answer2 = question2('pass sample input');
-const answer3 = question3('pass sample input');
+
+
+const answer1 = valArray.filter(question1);
+ const answer2 = library.sort(question2);
+ const answer3 = question3(25468);
 
 console.log(answer1);
-console.log(answer2);
-console.log(answer3);
+ console.log(answer2);
+ console.log(answer3);
